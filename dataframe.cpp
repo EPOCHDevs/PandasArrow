670eb0f495ad35f25fc7e55dcdaa6affee5f1f7d
dewe <ava6969@rit.edu>
2023-05-16 13:49:31 -0700

    tabulate version needs update

--- dataframe.cpp
+++ dataframe.cpp
@@ -271,8 +271,8 @@ std::ostream& operator<<(std::ostream& os, DataFrame const& df)
     const auto halfRow = int(PD_MAX_ROW_TO_PRINT / 2);
     const auto halfCol = int(PD_MAX_COL_TO_PRINT / 2);
 
-    std::vector<std::variant<std::string, tabulate::Table>> dot{ halfRow, "" };
-    std::vector<std::variant<std::string, tabulate::Table>> cells;
+    std::vector<std::variant<std::string, const char*, tabulate::Table>> dot{ halfRow, "" };
+    std::vector<std::variant<std::string, const char*, tabulate::Table>> cells;
     cells.emplace_back("index");
     for (int j = 0; j < nCols; j++)
     {
